12ec49a25bf72abbc05712858549bbd64b62a1f6
agustin29reta <agustinrubenretamozoenrique@gmail.com>
2020-09-10 11:05:23 -0300

    plantilla de prueba

---
+++ Proyecto_Informatorio/voluntariado/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path
+from voluntariado import views
+
+urlpatterns = [
+      path('', views.home, name="Home"),
+      path('home/', views.home, name="Home")
+]
