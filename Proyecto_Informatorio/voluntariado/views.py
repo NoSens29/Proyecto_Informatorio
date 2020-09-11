e4ee59f77f5db3ea9a8bcd6bb1b3c266391cdeaf
vicgaba <victorgabardini@gmail.com>
2020-09-11 00:47:00 -0300

    Agrego contadores a home.html

--- Proyecto_Informatorio/voluntariado/views.py
+++ Proyecto_Informatorio/voluntariado/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from .models import Persona, Actividad
 
 # Create your views here.
 def home(request):
+	num_voluntarios = Persona.objects.filter(voluntario=True).count() #del modelo de Persona cuento todos los que están como voluntarios
+	num_favores = Actividad.objects.count() #del modelo de Actividad cuento todas las actividades cargadas
+	num_faltantes = Actividad.objects.filter(realizada=False).count()
 
-	return render(request, "voluntariado/home.html")
+	return render(request, "voluntariado/home.html",context={'num_voluntarios':num_voluntarios,'num_favores':num_favores,'num_faltantes':num_faltantes})
 
