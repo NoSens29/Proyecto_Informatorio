821d0b8ffd24a59e032430e4ff307642b76d1ce9
vicgaba <victorgabardini@gmail.com>
2020-09-12 00:24:35 -0300

    agrego template registrar voluntario y views de registro en la base

--- Proyecto_Informatorio/voluntariado/views.py
+++ Proyecto_Informatorio/voluntariado/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from django.template import loader
 from .models import Persona, Actividad
 
 # Create your views here.
@@ -9,3 +10,11 @@ def home(request):
 
 	return render(request, "voluntariado/home.html",context={'num_voluntarios':num_voluntarios,'num_favores':num_favores,'num_faltantes':num_faltantes})
 
+def registrar_voluntario(request):
+	if request.POST:
+		POST = request.POST
+		nuevo_registro = Persona(dni=POST['dni'], nombre=POST['name'],
+			apellido=POST['surname'], telefono=POST['phone'], mail=POST['email'], direccion=POST['adress'], voluntario=POST['voluntary'], administrador=POST['admin'], solicitante=POST['solicitant']) 
+		nuevo_registro.save()
+
+	return render(request, 'voluntariado/voluntario.html')
