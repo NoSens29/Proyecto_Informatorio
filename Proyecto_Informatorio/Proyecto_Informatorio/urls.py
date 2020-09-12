183588f0ee49e5c39031a5ce1de4598c5a09a8ff
agustin29reta <agustinrubenretamozoenrique@gmail.com>
2020-09-12 19:12:53 -0300

    nada de cambio

--- Proyecto_Informatorio/Proyecto_Informatorio/urls.py
+++ Proyecto_Informatorio/Proyecto_Informatorio/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('voluntariado/', include('voluntariado.urls'))
+    path('', include('voluntariado.urls'))
    
 ]
