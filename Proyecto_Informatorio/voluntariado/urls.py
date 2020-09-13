3f38f5e96b6617e1af55647f580ae0735f338e40
soteloagustinn <soteloagustin42@gmail.com>
2020-09-13 20:02:31 -0300

    agregue el boton login y el link del boton registro

--- Proyecto_Informatorio/voluntariado/urls.py
+++ Proyecto_Informatorio/voluntariado/urls.py
@@ -6,6 +6,8 @@ from voluntariado import views
 urlpatterns = [
       path('', views.home, name="Home"),
  #     path('home/', views.home, name="Home"),
-      path('registrar_voluntario/', views.registrar_voluntario, name='registrar_voluntario'),
-      path('registrar_solicitante/', views.registrar_solicitante, name='registrar_solicitante')
+      path('registro/', views.registro),
+      path('login/', views.login),
+      path('registro/', views.registrar_voluntario, name='registrar_voluntario'),
+      path('registro/', views.registrar_solicitante, name='registrar_solicitante')
 ]
