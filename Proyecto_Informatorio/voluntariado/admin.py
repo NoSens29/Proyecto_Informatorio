c920977814629420630055ecc85382d40ff8a0d3
agustin29reta <agustinrubenretamozoenrique@gmail.com>
2020-09-14 12:52:01 -0300

    ll

--- Proyecto_Informatorio/voluntariado/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from voluntariado.models import Persona, Actividad
-
-# Register your models here.
-
-admin.site.register(Persona)
-admin.site.register(Actividad)
--- Proyecto_Informatorio/voluntariado/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class VoluntariadoConfig(AppConfig):
-    name = 'voluntariado'
