8225fd20c4daf2bb010eb5db4a584dc1949234f9
rodrialmiron <rodri.22.10.14@gmail.com>
2020-09-20 13:56:44 -0300

    cambio

---
+++ Proyecto_Informatorio/Proyecto_Informatorio/voluntariado/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Persona, Actividad, MiUsuario
+from django.contrib.auth.forms import UserCreationForm
+
+class PersonaForm(forms.ModelForm):
+
+    class Meta:
+        model = Persona
+        fields = ('dni','nombre','apellido','usuario','telefono','mail','direccion','contrasenia','solicitante','voluntario')
+
+
+class MiUsuarioCreationForm(UserCreationForm):
+
+    class Meta(UserCreationForm.Meta):
+        model = MiUsuario
+        fields = ('mi_usuario','dni', 'voluntario', 'solicitante')
+
+class ActividadForm(forms.ModelForm):
+	class Meta:
+		model = Actividad
+		fields =('nombre','fecha','realizada','id_solicitante')
--- Proyecto_Informatorio/Proyecto_Informatorio/voluntariado/views.py
+++ Proyecto_Informatorio/Proyecto_Informatorio/voluntariado/views.py
@@ -1,10 +1,124 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.template import loader
+from .models import Persona, Actividad, MiUsuario
+from .forms import PersonaForm, MiUsuarioCreationForm, ActividadForm
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as do_login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
+	num_voluntarios = Persona.objects.filter(voluntario=True).count() #del modelo de Persona cuento todos los que están como voluntarios
+	num_favores = Actividad.objects.count() #del modelo de Actividad cuento todas las actividades cargadas
+	num_faltantes = Actividad.objects.filter(realizada=False).count()
 
-	return render(request, "voluntariado/home.html")
+	return render(request, "voluntariado/home.html",context={'num_voluntarios':num_voluntarios,'num_favores':num_favores,'num_faltantes':num_faltantes})
 
-def solicitud(request):
+def registrar_voluntario(request):
+	if request.POST:
+		POST = request.POST
+		nuevo_registro = Persona(dni=POST['dni'], nombre=POST['name'],
+			apellido=POST['surname'], telefono=POST['phone'], mail=POST['email'], direccion=POST['adress'], voluntario=POST['voluntary'], 
+			administrador=POST['admin'], solicitante=POST['solicitant'], contrasenia=POST['password'],usuario=POST['username']) 
+		nuevo_registro.save()
+		
+	return render(request, 'voluntariado/voluntario.html')
 
-	return render(request, "voluntariado/solicitante.html")
+def registrar_persona(request):
+	form = PersonaForm
+	return render(request, 'voluntariado/persona_new.html', {'form': form})
+
+def registrar_solicitante(request):
+	if request.POST:
+		POST = request.POST
+		nuevo_registro = Persona(dni=POST['dni'], nombre=POST['name'],
+			apellido=POST['surname'], telefono=POST['phone'], mail=POST['email'], direccion=POST['adress'], voluntario=POST['voluntary'], administrador=POST['admin'], solicitante=POST['solicitant'], contrasenia=['password']) 
+		nuevo_registro.save()
+
+	return render(request, 'voluntariado/solicitante.html')
+
+def registro(request):
+	return render(request, 'voluntariado/registro.html')
+
+def login(request):
+	if request.POST:
+		data = request.POST
+		user = data['username']
+		password = data['password']
+		try:
+			persona = Persona.objects.get(usuario=user)
+			if password == persona.contrasenia:
+				return redirect('Home')
+		except:
+			pass
+	return render(request, 'voluntariado/login.html')
+
+def Historiadefavores(request):
+	return render(request,'voluntariado/Historiadefavores.html')
+
+def contacto(request):
+	return render(request,'voluntariado/contacto.html')
+
+def donaciones(request):
+	return render(request,"voluntariado/donaciones.html")
+
+
+def login2(request):
+    # Creamos el formulario de autenticación vacío
+    form = AuthenticationForm()
+    if request.method == "POST":
+        # Añadimos los datos recibidos al formulario
+        form = AuthenticationForm(data=request.POST)
+        # Si el formulario es válido...
+        if form.is_valid():
+            # Recuperamos las credenciales validadas
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            # Verificamos las credenciales del usuario
+            user = authenticate(username=username, password=password)
+
+            # Si existe un usuario con ese nombre y contraseña
+            if user is not None:
+                # Hacemos el login manualmente
+                do_login(request, user)
+                # Y le redireccionamos a la portada
+                return redirect('Home')
+
+    # Si llegamos al final renderizamos el formulario
+    return render(request, "voluntariado/login2.html", {'form': form})
+
+def registro_miusuario(request):
+	# Creamos el formulario de autenticación vacío
+	form = MiUsuarioCreationForm()
+	if request.method == "POST":
+		# Añadimos los datos recibidos al formulario
+		form = MiUsuarioCreationForm(data=request.POST)
+		# Si el formulario es válido...
+		if form.is_valid():
+			# Creamos la nueva cuenta de usuario
+			user = form.save()
+			# Si el usuario se crea correctamente
+			if user is not None:
+				# Hacemos el login manualmente
+				do_login(request, user)
+				# Y le redireccionamos a la portada
+				return redirect('/')
+	# Si llegamos al final renderizamos el formulario
+	return render(request, "voluntariado/registro_miusuario.html", {'form':form})
+
+def registro_actividad(request):
+	#Establezco que formulario voy a utilizar
+	form = ActividadForm
+	#Consulto si el request viene con post o es la primera vez que se ejecuta
+	if request.method == "POST":
+		#Añadimos los datos recibidos a la variable form
+		form = ActividadForm(data=request.POST)
+		#consultamos si el formulario es válido
+		if form.is_valid():
+			#registramos la actividad
+			actividad = form.save()
+
+			return redirect('Home')
+	return render(request, "voluntariado/registro_actividad.html", {'form':form})
