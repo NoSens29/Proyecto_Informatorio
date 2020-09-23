bf10922ffb9af2c564b0881d3925540bbd560b81
agustin29reta <agustinrubenretamozoenrique@gmail.com>
2020-09-23 19:10:51 -0300

    fixed, rodrigo hay que borrar tu branch y tus carpetas

--- primer_ejercicio.py
+++
@@ -1,8 +0,0 @@
-print("hola mundo")
-print("hola versionado")
-print("cambios agregados en la nueva rama")
-print("esto solo se encuentra en la nueva rama")
-print("esto se agrega en un segundo commit")
-print("esto en la rama2. que onda")
-print("cambios nuevos de la rama2")
-print("cambios a eliminar")
